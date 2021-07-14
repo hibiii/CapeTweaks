747fce80514bc3a7c4a5e3f8aea30e1199d04c48
Hibiii <69405847+Hibiii@users.noreply.github.com>
2021-07-14 05:29:19 -0300

    Add option for capes to not texture the elytra

---
+++ src/main/java/hibi/capetweaks/mixin/ElytraFeatureRendererMixin.java
@@ -0,0 +1,35 @@
+package hibi.capetweaks.mixin;
+
+import net.minecraft.client.network.AbstractClientPlayerEntity;
+import net.minecraft.client.render.entity.feature.ElytraFeatureRenderer;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+import hibi.capetweaks.Config;
+
+@Mixin(ElytraFeatureRenderer.class)
+public class ElytraFeatureRendererMixin {
+	@Redirect(
+		method = "render(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;ILnet/minecraft/entity/Entity;FFFFFF)V",
+		at = @At(
+			value = "INVOKE",
+			target = "Lnet/minecraft/client/AbstractClientPlayerEntity;canRenderElytraTexture()Z"
+		)
+	)
+	private boolean canElytra(AbstractClientPlayerEntity acpe) {
+		return Config.elytraTexture && acpe.canRenderElytraTexture();
+	}
+
+	@Redirect(
+		method = "render(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;ILnet/minecraft/entity/Entity;FFFFFF)V",
+		at = @At(
+			value = "INVOKE",
+			target = "Lnet/minecraft/client/AbstractClientPlayerEntity;canRenderCapeTexture()Z"
+		)
+	)
+	private boolean canCape(AbstractClientPlayerEntity acpe) {
+		return Config.elytraTexture && acpe.canRenderCapeTexture();
+	}
+}
